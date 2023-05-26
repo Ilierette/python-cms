6c3e41df86635149b64823ef986050f46198fd67
Karolina Pietraś <Ilierette@users.noreply.github.com>
2023-05-26 22:31:59 +0200

    instagram feed

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'links',
+    'corsheaders',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -46,6 +47,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +59,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'cms.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    env("CORS_ORGIN"),
+]
 
 TEMPLATES = [
     {
--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = async () => {
+    const response = await fetch(`${import.meta.env.VITE_API}/events/`)
+    const jsonData = await response.json();
+    console.log(jsonData);
+  }
 
   return (
     <>
